2d7f81ab2833a365e4d8631c130bef928e0ac272
David Leder <davidleder98@gmail.com>
2024-11-13 20:19:22 +0100

    bs 3

--- src/components/bundle/Bundle.tsx
+++ src/components/bundle/Bundle.tsx
@@ -96,6 +96,13 @@ const Bundle: React.FC<BundleProps> = ({ id }) => {
                     slidesToShow: 3,
                     slidesToScroll: 1,
                 },
+            },
+            {
+                breakpoint: 700,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                },
             }
         ],
     };
