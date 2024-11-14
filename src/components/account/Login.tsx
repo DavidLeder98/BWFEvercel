942062f669da01026eadf885e22abf03b64a86da
David Leder <davidleder98@gmail.com>
2024-11-14 18:34:39 +0100

    ac 7

--- src/components/account/Login.tsx
+++ src/components/account/Login.tsx
@@ -43,10 +43,7 @@ const Login = () => {
         onChange={(e) => setPassword(e.target.value)}
         className="account-input"
       />
-      <Link to="/register" className="reg-link">
-       <p>Don't have an account?</p>
-       <p>Create one now!</p>
-      </Link>
+      <Link to="/register" className="reg-link">Don't have an account?</Link>
       <div className="albc">
         <Button text="Login" onClick={handleLogin} variant="filled" color="blue" />
       </div>
