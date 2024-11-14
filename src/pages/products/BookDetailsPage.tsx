794b57290dc227b3628115509836fd6950b4a9d1
David Leder <davidleder98@gmail.com>
2024-11-14 23:19:26 +0100

    scroll issue

--- src/pages/products/BookDetailsPage.tsx
+++ src/pages/products/BookDetailsPage.tsx
@@ -5,15 +5,15 @@ import Bundle from "../../components/bundle/Bundle";
 import { useEffect } from 'react';
 
 const BookDetailsPage: React.FC = () => {
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, []);
-    
     const { id } = useParams<{ id: string }>(); // Use the useParams hook to get the book ID
-    const bookId = id ? parseInt(id, 10) : undefined; // Convert the ID to a number, or set it to null if undefined
+    const bookId = id ? parseInt(id, 10) : undefined; // Convert the ID to a number, or set it to undefined if invalid
+
+    useEffect(() => {
+        window.scrollTo(0, 0); // Scroll to top whenever id changes
+    }, [id]); // Trigger this effect every time the book ID changes
 
     if (bookId === undefined) {
-        return <Navigate to="/404" />; // Adjust the route as needed
+        return <Navigate to="/404" />; // Redirect to a 404 page if no valid ID is provided
     }
 
     return (
