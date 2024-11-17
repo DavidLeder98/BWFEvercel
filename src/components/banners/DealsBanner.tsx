db8305064c52cc263f21e91ff553899809d2610a
David Leder <davidleder98@gmail.com>
2024-11-17 19:06:24 +0100

    banner 2

--- src/components/banners/DealsBanner.tsx
+++ src/components/banners/DealsBanner.tsx
@@ -1,12 +1,12 @@
 import './Banner.css';
-import spook from '../../assets/banners/spook.png';
+import deals from '../../assets/banners/deals.png';
 import { Link } from 'react-router-dom';
 
 const DealsBanner = () => {
     return (
         <div className="banner-container">
             <Link to="/deals">
-                <img className="static-img-l" src={spook} />
+                <img className="static-img-l" src={deals} />
             </Link>
         </div>
     )
