f1df505ec9807d799166d3e3e331cbf0a0d55c7c
David Leder <davidleder98@gmail.com>
2024-11-18 02:13:47 +0100

    banners 8

--- src/components/banners/AuthorsBanner.tsx
+++ src/components/banners/AuthorsBanner.tsx
@@ -1,5 +1,6 @@
 import './Banner.css';
 import bestsellers from '../../assets/banners/bestsellers.png';
+import bestsellersmobile from '../../assets/banners/bestsellersmobile.png';
 import { Link } from 'react-router-dom';
 
 const AuthorsBanner = () => {
@@ -7,6 +8,7 @@ const AuthorsBanner = () => {
         <div className="banner-container">
             <Link to="/bestsellers">
                 <img className="static-img-l" src={bestsellers} />
+                <img className="static-img-s" src={bestsellersmobile} />
             </Link>
         </div>
     )
