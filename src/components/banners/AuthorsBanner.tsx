ae60726418bdb5ef463480192187901cc5509c52
David Leder <davidleder98@gmail.com>
2024-11-18 01:45:09 +0100

    banners 5

--- src/components/banners/AuthorsBanner.tsx
+++ src/components/banners/AuthorsBanner.tsx
@@ -1,12 +1,12 @@
 import './Banner.css';
-import authorsbanner from '../../assets/banners/authors.png';
+import bestsellers from '../../assets/banners/bestsellers.png';
 import { Link } from 'react-router-dom';
 
 const AuthorsBanner = () => {
     return (
         <div className="banner-container">
             <Link to="/bestsellers">
-                <img className="static-img-l" src={authorsbanner} />
+                <img className="static-img-l" src={bestsellers} />
             </Link>
         </div>
     )
--- src/components/banners/DealsBanner.tsx
+++ src/components/banners/DealsBanner.tsx
@@ -1,5 +1,6 @@
 import './Banner.css';
 import deals from '../../assets/banners/deals.png';
+import dealsmobile from '../../assets/banners/dealsmobile.png';
 import { Link } from 'react-router-dom';
 
 const DealsBanner = () => {
@@ -7,6 +8,7 @@ const DealsBanner = () => {
         <div className="banner-container">
             <Link to="/deals">
                 <img className="static-img-l" src={deals} />
+                <img className="static-img-s" src={dealsmobile} />
             </Link>
         </div>
     )
